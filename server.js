5eb1d0c2c6654d40e37dfdcd20af850d9f75f476
wcrvtt <watcharavit.ta@gmail.com>
2023-02-09 16:18:43 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config ({path:'./config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Route files
+const hospitals = require("./routes/hospitals");
 const app = express();
 
-const hospitals = require("./routes/hospitals");
-app.use("/api/v1/hospitals",hospitals);
+// Body parser
+app.use(express.json());
+
+app.use('/api/v1/hospitals',hospitals);
 
 const PORT = process.env.PORT||5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(()=>process.exit(1));
+});
