852072e9d4ecacf48743f62acfb848bef5be8315
wcrvtt <watcharavit.ta@gmail.com>
2023-02-22 20:33:06 +0700

    add Authentication

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {getHospitals, getHospital, createHospital, updateHospital,deleteHospital} = require('../controllers/hospitals')
+const {getHospitals, getHospital, createHospital, updateHospital,deleteHospital} = require('../controllers/hospitals');
 const router = express.Router();
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
+router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db')
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config ({path:'./config/config.env'});
@@ -10,12 +11,17 @@ connectDB();
 
 // Route files
 const hospitals = require("./routes/hospitals");
+const auth = require('./routes/auth');
 const app = express();
 
 // Body parser
 app.use(express.json());
 
+// Cookie parser
+app.use(cookieParser());
+
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT||5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
